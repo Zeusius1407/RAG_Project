edd49b6bf0688b872120eb39e9a7c130b82d7087
Tejas Agarwal <tejasagarwal@Tejass-Laptop-2.local>
2025-08-27 17:08:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import numpy as np
 
 
 embed_model = SentenceTransformer("intfloat/e5-base-v2")
-#genai.configure(api_key = os.environ["API_KEY_HERE"])
+genai.configure(api_key = os.environ["GEMINI_API_KEY"])
 PDF_PATH = "./dataset/Inductions Catalogue.pdf"
 
 client = chromadb.PersistentClient(path="./RAG_db")
@@ -58,8 +58,28 @@ def retrieve_relevant_chunks(query, embedding_model, client, top_k=20):
     relevant_chunks = results['documents'][0]
     return relevant_chunks
 
+def generate_answer(query, relevant_chunks):
+    context = '\n'.join(relevant_chunks)
+    prompt_template = f"""
+    Answer the question based only on the provided context. If you cannot answer the question from the context, say "I'm sorry, I dont know"
+    Context:
+    ---
+    {context}
+    ---
+    Question:
+    ---
+    {query}
+    ---
+    """
+    model = genai.GenerativeModel('gemini-2.5-flash')
+    response = model.generate_content(prompt_template)
+    return response.text
+
+
 chunks = chunk_pdf(pdf_path=PDF_PATH, chunk_size=30)
 embeddings = embed(chunks=chunks, embed_model=embed_model)
 create_vector_store(embeddings=embeddings, chunks=chunks, client=client)
 query = input("Enter your query: ")
 retrieved_relevant_chunks = retrieve_relevant_chunks(query=query, embedding_model=embed_model, client=client)
+augmented_response = generate_answer(query=query, relevant_chunks=retrieved_relevant_chunks)
+print(augmented_response)
